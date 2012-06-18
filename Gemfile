7b695d138a3dd7fedbf8ace6cd2588886e2bbd4c
Jennifer Cwagenberg <jennifercwagenberg@gmail.com>
2012-06-18 02:50:20 -0500

    made changes for the homework

--- Gemfile
+++ Gemfile
@@ -14,9 +14,10 @@ group :development, :test do
   gem 'database_cleaner'
   gem 'capybara'
   gem 'launchy'
+  gem 'rspec-rails'
 end
 group :production do
-#  gem 'pg'
+  gem 'pg'
 end
 
 # Gems used only for assets and not required
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  
+  assert movies_table.hashes.count == Movie.all.count
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,8 +16,14 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #print page.html
+  movie_titles = page.all(:xpath, '//table/tbody/tr/td[1]/text()').map {|title| title.text}
+  index_e1 = movie_titles.index(e1).to_i  
+  index_e2 = movie_titles.index(e2).to_i
+  
+  assert index_e1 != 0 and index_e2 != 0 and index_e1 < index_e2
 end
+
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
@@ -25,4 +33,25 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+    rating_list.split(",").each do |rating| 
+      step %Q{I #{uncheck}check "ratings_#{rating.strip}"}
+    end
+      
+end
+
+
+#Then /I should( not)? see all of the movies: (.*)/ do |ifnot, movies_list|
+Then /I should( not)? see all of the movies/ do |all_or_none|
+  movies = page.all(:xpath, '//table/tbody/tr/td[1]/text()').map {|movie| movie.text}
+  
+  if all_or_none == " not"
+    assert movies.count == 0
+  else
+    assert movies.count == Movie.all.count
+  end
+end
+
+Then /^I should see the following ratings: (.*)/ do |rating_list|
+    movie_ratings = page.all(:xpath, '//table/tbody/tr/td[2]/text()').map {|rating| rating.text}
+    rating_list.split(",").each {|rating| assert movie_ratings.include?(rating.strip)}
 end
